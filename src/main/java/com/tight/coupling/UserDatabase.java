3b511b0454a3e653cb8daee6266ca9f061edf930
bgorantla <bgorantla500@gmail.com>
2026-03-14 14:48:42 -0700

    First commit

---
+++ src/main/java/com/tight/coupling/UserDatabase.java
@@ -0,0 +1,8 @@
+package com.tight.coupling;
+
+public class UserDatabase {
+    public String getUserDetails(){
+        //Directly acess database
+        return "User Details From Database";
+    }
+}
